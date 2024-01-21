3eb719ba04508649e0b568dec0fcba245eac675d
alexey <alex@mrk.digital>
2024-01-21 11:05:50 +0300

    21 01 2024 new

--- src/components/CallUs/CallUS.js
+++ src/components/CallUs/CallUS.js
@@ -1,11 +1,9 @@
 import * as React from 'react';
 import {createTheme, ThemeProvider} from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
-import ImageMain from "../../img/main-baby.png";
 import TelegramIcon from '@mui/icons-material/Telegram';
 import Link from "@mui/material/Link";
 
